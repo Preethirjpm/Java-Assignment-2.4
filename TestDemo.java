c7d9cbbb9203fd53dbb604f8d0bedfc86d2652e0
Preethirjpm <preethirjpm@gmail.com>
2017-07-02 22:28:19 +0530

    Java Assignment 2.4(a)

---
+++ TestDemo.java
@@ -0,0 +1,24 @@
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	int n1 = Integer.parseInt(args[0]);
+	
+	if(n1 == 0)
+	
+	System.out.println("You have entered ZERO ");
+	
+	else if (n1 < 0)
+	
+	System.out.println("You have entered Negetive value ");
+	
+	else 
+	
+	System.out.println("You have entered positive value ");
+			
+	}
+
+}
