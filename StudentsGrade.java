dde42133fd036c921e8f09ce34798176389c741d
Preethirjpm <preethirjpm@gmail.com>
2017-07-03 20:48:13 +0530

    Assignment 2.4- students grade

---
+++ StudentsGrade.java
@@ -0,0 +1,35 @@
+
+public class StudentsGrade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	   int marks= 100; 
+	   int age = 15;
+	   
+	   if( marks >=70 && marks >=60 )
+		   if(age < 15)
+	   {  
+	   System.out.println(marks >=70 && age < 15);  
+	    }  
+	   else if( marks >= 61 && marks <= 70)
+	   if (marks >= 45 && marks <=60)
+		   if(age < 15){
+        System.out.println( marks >= 45 && age < 15);  
+			    }  
+	    else if( marks < 61 && marks < 45)
+	    if (age < 15){  
+		System.out.println( marks < 45 && age < 15);  
+			    }  
+	    else{  
+	    	
+	        System.out.println("A grade");  
+	        System.out.println("B grade");  
+	        System.out.println("C grade");  
+
+
+			    }  
+			 
+	}
+
+}
